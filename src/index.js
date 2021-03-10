5e14d95a0482bb9147d4bbe6c6c94b1bd5553feb
Запитецкая Алена Евгеньевна <190032@edu.psuti.ru>
2021-03-10 15:08:07 +0400

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.join('').replace(/,/g, '');
+    let stack = [];
+    for (let bracket of str) {
+        let bracketsIndex = brackets.indexOf(bracket)
+
+        if (bracketsIndex % 2 === 0) {
+
+            if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){
+                stack.pop();
+            } else if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+                stack.push(bracketsIndex)
+            }
+            else{
+                stack.push(bracketsIndex)
+            }
+        }
+        else {
+            if (stack.pop() !== bracketsIndex-1){
+                return false;
+            }
+        }
+
+    }
+    return stack.length === 0
 }
